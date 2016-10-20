f5a92db59968bf2ecdb8c8f021ef0c9ea88c1c55
mhu <marvin.hu@autotrader.com>
2016-10-19 20:55:55 -0400

    update local host

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 
@@ -18,4 +18,4 @@ app.use(express.static('public'));
 
 app.listen(PORT, function(){
   console.log('express serve is up on port ' + PORT);
-})
+});
