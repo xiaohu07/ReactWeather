b0224fdbefbc1e54c23fd3a8e8b92ab1e6ce888b
mhu <marvin.hu@autotrader.com>
2016-10-20 22:13:42 -0400

    Update about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,26 @@
 var React = require('react');
-
+var { Link } = require('react-router');
 
 var About = (props) => {
   return (
-    <div>
-      <h3>About</h3>
-      <p>Welcome to Heroku</p>
+    <div className="">
+      <div className="">
+        <h1 className="text-center">About</h1>
+        <p>Welcome to Heroku</p>
+        <Link to="https://github.com/xiaohu07"><span className="success badge">1</span> Github </Link>
+        <br/>
+        <Link to="https://facebook.com"><span className="success badge">2</span> Github </Link>
+       
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a> -- This was the JS framework used.
+          </li>
+          <li>
+            <a href="http://openweathermap.org">Open Weather Map</a> -- Used to search weather by city.
+          </li>
+        </ul>
+      </div>
+      
     </div>
       
   
